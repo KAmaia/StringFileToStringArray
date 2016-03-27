914225536a5bf280494dfe49122ffa9155bea63f
Kamaia <krystalamaia@gmail.com>
2016-03-26 23:53:32 -0500

    initial commit

---
+++ nameTextToArray/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameTextToArray {
+	public class Converter {
+		private List<string> names;
+		public Converter(string fileName ) {
+			names = new List<string>( );
+			ReadFile( fileName );
+			SaveFile( fileName );
+		}
+		private void SaveFile(string fileName ) {
+			string newFileName = fileName.Remove(fileName.LastIndexOf('.'));
+			newFileName += "_converted.txt";
+			File.WriteAllLines( newFileName, names.ToArray( ) );
+		}
+		private void ReadFile( string fileName ) {
+			string[] rawData = File.ReadAllLines( fileName );
+			
+			foreach(string s in rawData ) {
+				string nameToAdd = "\"" + s.Split('\t')[0] + "\",";
+				names.Add( nameToAdd );
+			}
+		}
+	}
+
+}
---
+++ nameTextToArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameTextToArray {
+	class Program {
+		static void Main( string[ ] args ) {
+			Converter c = new Converter(args[0]);
+		}
+	}
+}
